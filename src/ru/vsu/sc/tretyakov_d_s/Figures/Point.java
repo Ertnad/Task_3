7de8ea5f9f4cc5e755b4bbd22ec6bdb3a904b2f1
Ertnad <mrgerchannel@gmail.com>
2021-10-01 21:38:52 +0300

    v1.3

---
+++ src/ru/vsu/sc/tretyakov_d_s/Figures/Point.java
@@ -0,0 +1,11 @@
+package ru.vsu.sc.tretyakov_d_s.Figures;
+
+public class Point {
+  public double x;
+  public double y;
+
+  public Point(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
+}
